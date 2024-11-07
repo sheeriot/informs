559ce53eee696bb40aa8922317f3c9606d1e866d
Kris Thompson <kris@austinvoicedata.com>
2024-11-07 14:14:01 +0000

    now with export to CSV

--- informs/webapp/aidrequests/views/aidrequest.py
+++ informs/webapp/aidrequests/views/aidrequest.py
@@ -2,12 +2,11 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.db.models import Count
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import AidRequest, RegionResponse
-from .forms import AidRequestForm, RegionResponseForm
+from ..models import AidRequest, RegionResponse
+from .forms import AidRequestForm
 
-from icecream import ic
+# from icecream import ic
 
 
 # List View for AidRequests
@@ -39,7 +38,6 @@ class AidRequestDetailView(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         regionresponse = get_object_or_404(RegionResponse, slug=rrslug)
         context['regionresponse'] = regionresponse
-        ic(regionresponse)
         return context
 
 
@@ -105,58 +103,8 @@ class AidRequestDeleteView(LoginRequiredMixin, DeleteView):
         return reverse_lazy('aidrequest_list', kwargs={'regionresponse': self.kwargs['regionresponse']})
 
     def get_context_data(self, **kwargs):
-        ic(kwargs)
         rrslug = self.kwargs['regionresponse']
         context = super().get_context_data(**kwargs)
         regionresponse = get_object_or_404(RegionResponse, slug=rrslug)
         context['regionresponse'] = regionresponse
         return context
-
-
-# List View for RegionResponse
-class RegionResponseListView(LoginRequiredMixin, ListView):
-    model = RegionResponse
-    template_name = 'aidrequests/regionresponse_list.html'
-
-    def get_queryset(self):
-        queryset = RegionResponse.objects.annotate(aidrequest_count=Count('aidrequest'))
-        return queryset
-
-
-# Detail View for RegionResponse
-class RegionResponseDetailView(LoginRequiredMixin, DetailView):
-    model = RegionResponse
-    template_name = 'aidrequests/regionresponse_detail.html'
-
-
-# Create View for RegionResponse
-class RegionResponseCreateView(LoginRequiredMixin, CreateView):
-    model = RegionResponse
-    form_class = RegionResponseForm
-    template_name = 'aidrequests/regionresponse_form.html'
-    success_url = reverse_lazy('regionresponse_list')
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['action'] = 'create'  # Pass 'create' action to the form
-        return kwargs
-
-
-# Update View for RegionResponse
-class RegionResponseUpdateView(LoginRequiredMixin, UpdateView):
-    model = RegionResponse
-    form_class = RegionResponseForm
-    template_name = 'aidrequests/regionresponse_form.html'
-    success_url = reverse_lazy('regionresponse_list')
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['action'] = 'update'  # Pass 'update' action to the form
-        return kwargs
-
-
-# Delete View for RegionResponse
-class RegionResponseDeleteView(LoginRequiredMixin, DeleteView):
-    model = RegionResponse
-    template_name = 'aidrequests/regionresponse_confirm_delete.html'
-    success_url = reverse_lazy('regionresponse_list')
